d63451edb94e1f3926e0a6239d435e046086c0a6
Teapack1 <ondrej.major@gmail.com>
2024-06-21 00:32:11 +0200

    all warehouses in one timeline

--- working.py
+++ working.py
@@ -2,39 +2,31 @@ from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 import joblib
 import pandas as pd
 
-def preprocess_data(dataframe, categorical_columns, numerical_columns, date_columns, loaded_oh_encoder, loaded_num_scaler, loaded_date_scaler):
+def preprocess_data(dataframe, x_scaler, ohsc, cat_col):
 
     # Convert the date column to datetime
     dataframe['date'] = pd.to_datetime(dataframe['date'])
     
     # Encode categorical features
-    encoded_categorical_features = loaded_oh_encoder.transform(dataframe[categorical_columns])
-    encoded_feature_names = loaded_oh_encoder.get_feature_names_out(categorical_columns)
+    encoded_categorical_features = ohsc.transform(dataframe[cat_col])
+    encoded_feature_names = ohsc.get_feature_names_out(cat_col)
     dataframe_encoded = pd.DataFrame(encoded_categorical_features, columns=encoded_feature_names, index=dataframe.index)
     if 'holiday_name_nan' in dataframe_encoded.columns:
         dataframe_encoded = dataframe_encoded.drop(columns=['holiday_name_nan'])
+    
+    
     
     # Concatenate the encoded features with the original DataFrame
     dataframe = pd.concat([dataframe, dataframe_encoded], axis=1)
     
     # Drop the original categorical columns
-    dataframe = dataframe.drop(columns=categorical_columns)
+    dataframe = dataframe.drop(columns=cat_col)
     
     # Extract date features
     dataframe.index = dataframe.pop('date')
-    dataframe['year'] = dataframe.index.year
-    dataframe['month'] = dataframe.index.month
-    dataframe['day'] = dataframe.index.day
-    dataframe['day_of_week'] = dataframe.index.dayofweek
-    dataframe['is_weekend'] = dataframe['day_of_week'].isin([5, 6]).astype(int)
-    
-    # Scale numerical features
-    if numerical_columns:
-        dataframe[numerical_columns] = loaded_num_scaler.transform(dataframe[numerical_columns])
-    
-    # Scale date features
-    if date_columns:
-        dataframe[date_columns] = loaded_date_scaler.transform(dataframe[date_columns])
+
+    df_trans = x_scaler.transform(dataframe)
+    dataframe = pd.DataFrame(df_trans, columns=dataframe.columns)
     
     
     
